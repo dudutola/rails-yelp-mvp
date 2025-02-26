1e0a6abc35f94c5a73cc7534503e5832dfa215ec
Dulcelene Machado <dudutola@gmail.com>
2025-02-26 10:00:18 +0000

    Added review model and its migration Added has_many in Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_094717) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_26_095537) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,4 +19,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_26_094717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
